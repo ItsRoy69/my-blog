b91b8bb1f4b73d4a3976a418b04a95fb3843c0de
ItsRoy69 <jyotirmoyroy649@gmail.com>
2022-01-09 14:55:49 +0530

    Updated

--- client/src/components/Home/Post.js
+++ client/src/components/Home/Post.js
@@ -1,44 +1,45 @@
 import { Box, Typography, makeStyles } from '@material-ui/core';
 
-const useStyle = makeStyles({
-    container: {
-        border: '1px solid #d3cede',
-        borderRadius: 10,
-        margin: 10,
-        display: 'flex',
-        alignItems: 'center',
-        flexDirection: 'column',
-        height: 350,
-        '& > *': {
+const useStyles = makeStyles({
+  container: {
+      border: '1px solid #d3cede',
+      borderRadius: 10,
+      margin: 10,
+      display: 'flex',
+      alignItems: 'center',
+      flexDirection: 'column',
+      height: 350,
+      '& > *': {
             padding: '0 5px 5px 5px'
         }
-    },
-    image: {
-        width: '100%',
-        objectFit: 'cover',
-        borderRadius: '10px 10px 0 0',
-        height: 150
-    },
-    textColor: {
-        color: '#878787',
-        fontSize: 12
-    },
-    heading: {
-        fontSize: 18,
-        fontWeight: 600
-    },
-    detail: {
-        fontSize: 14,
-        wordBreak: 'break-word'
-    }
+  },
+  image: {
+    width: '100%',
+    objectFit: 'cover',
+    height: 150,
+    borderRadius: '10px 10px 0 0',
+  },
+  textColor: {
+      color: '#878787',
+      fontSize: 12
+  },
+  heading: {
+      fontSize: 18,
+      fontWeight: 600
+  },
+  detail: {
+      fontSize: 14,
+      wordBreak: 'break-word'
+  }
 })
 
 const Post = () =>  {
-  const classes = useStyle();
+  const classes = useStyles();
   const url = "https://images.unsplash.com/photo-1498050108023-c5249f4df085?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=752&q=80"
+
   return (
     <Box className={classes.container}>
-      <img src={url} alt="wrapper" />
+      <img src={url} alt="wrapper" className={classes.image} />
       <Typography className={classes.textColor}>Music</Typography>
       <Typography className={classes.heading}>Jyotirmoy Roy</Typography>
       <Typography className={classes.textColor}>Author: Roy</Typography>
--- client/src/components/Home/Posts.js
+++ client/src/components/Home/Posts.js
@@ -1,9 +1,14 @@
 import Post from './Post';
+import { Grid, Box } from '@material-ui/core';
 
 const Posts = () =>   {
-
+  let posts = [1,2,3,4,5,6,7,8]
   return (
-    <Post />
+    posts.map(post => (
+      <Grid item lg={3} sm={4} xs={12}>
+        <Post />
+      </Grid>
+    ))
   )
 }
 
