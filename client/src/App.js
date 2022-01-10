08cf413cc5f6bf9cd50013aa59b3594e810466d6
ItsRoy69 <jyotirmoyroy649@gmail.com>
2022-01-10 21:28:14 +0530

    Updated

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,27 @@
 import React from 'react';
 import { Box } from '@material-ui/core';
 
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+
 //components
 import Header from './components/Header';
 import Home from './components/Home/Home';
+import DetailView from './components/Post/DetailView';
 
 function App() {
   return (
-    <>
+    <BrowserRouter>
+
       <Header />
        <Box style={{marginTop: 64}}>
-      <Home />
+         <Routes>
+           <Route exact path='/' element={<Home/>} />
+           <Route exact path='/details' element={<DetailView/>} />
+        </Routes>
       </Box>
-    </>
+
+    </BrowserRouter>
   );
 }
 
---
+++ client/src/components/Post/DetailView.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const DetailView =() =>  {
+  return (
+    <p>Hello</p>
+  )
+}
+
+export default DetailView
