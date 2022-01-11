4fd6c6f2fc6a4e3b11fcafa26ba9c8a56866a7d7
ItsRoy69 <jyotirmoyroy649@gmail.com>
2022-01-11 16:16:30 +0530

    Update DetailView.js

--- client/src/components/Post/DetailView.js
+++ client/src/components/Post/DetailView.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Box, makeStyles } from '@material-ui/core';
+import { Box, makeStyles, Typography } from '@material-ui/core';
 import { Delete, Edit } from '@material-ui/icons';
 
 const useStyle = makeStyles({
@@ -10,7 +10,27 @@ const useStyle = makeStyles({
         width: '100%',
         height: '50vh',
         objectFit: 'cover'
-    }
+      },
+      icons: {
+       float: 'right'
+     },
+     icon: {
+        margin: 5,
+        padding: 5,
+        border: '1px solid #878787',
+        borderRadius: 10
+    },
+    heading: {
+        fontSize: 38,
+        fontWeight: 600,
+        textAlign: 'center',
+        margin: '50px 0 10px 0',
+    },
+    author: {
+        color: '#878787',
+        display: 'flex',
+        margin: '20px 0',
+    },
 });
 
 
@@ -20,9 +40,15 @@ const DetailView =() =>  {
   return (
     <Box className={classes.container}>
       <img src={url} alt="banner" className={classes.image} />
-      <Box>
-        <Edit />
-        <Delete />
+      <Box className={classes.icons}>
+        <Edit className={classes.icon} />
+        <Delete className={classes.icon} color="error"/>
+      </Box>
+      <Typography className={classes.heading}> Title of Blog </Typography>
+
+      <Box className={classes.author}>
+          <Typography>Author: <span style={{fontWeight: 600}}>That's Roy</span></Typography>
+          <Typography style={{marginLeft: 'auto'}}>11 June 2022 </Typography>
       </Box>
     </Box>
   )
